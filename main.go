e072a80845fb7496f99be9060c6719c5e7cc847b
Nikita Podmarev <nick13795@mail.ru>
2024-03-09 20:42:56 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlerCyber(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("index.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/helloCyber", handlerCyber)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+
+}
---
+++ static/js_main.js
@@ -0,0 +1,13 @@
+let num = 56 //number
+let secondName = 'Ilya'
+
+secondName = 'max' 
+num = 16
+
+//alert(secondName)
+
+console.log('test', secondName)
+console.log(num + 10)
+
+let num2 = 10 +num
+console.log(num, num2)
