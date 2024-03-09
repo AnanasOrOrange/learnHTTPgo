af20fcbfacfb9d936adca5deced7c35efe0e48a2
Nikita Podmarev <nick13795@mail.ru>
2024-03-09 21:24:01 +0300

    add mainPage.html and hanlder for it

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learnHTTPgo
 For learning
+
++ There are example from book: Head first go; page 743
++ One html + css page
--- main.go
+++ main.go
@@ -18,11 +18,19 @@ func handlerCyber(writer http.ResponseWriter, request *http.Request) {
 	err = html.Execute(writer, nil)
 	check(err)
 }
+
+func mainPage(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("index.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/helloCyber", handlerCyber)
+	http.HandleFunc("/guestBook", mainPage)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 
