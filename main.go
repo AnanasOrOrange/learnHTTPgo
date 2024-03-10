dc4d5f0afd0f86801ccd9b2eb44c2d0822298e5d
Nikita Podmarev <nick13795@mail.ru>
2024-03-10 19:23:24 +0300

    added new page: add signature and new html for it

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check(err error) {
@@ -12,6 +14,21 @@ func check(err error) {
 	}
 }
 
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
 func handlerCyber(writer http.ResponseWriter, request *http.Request) {
 	html, err := template.ParseFiles("index.html")
 	check(err)
@@ -19,18 +36,37 @@ func handlerCyber(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
 func mainPage(writer http.ResponseWriter, request *http.Request) {
-	html, err := template.ParseFiles("index.html")
+	signatures := getStrings("signatures.txt")
+	html, err := template.ParseFiles("mainPage.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+func addSignature(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
 	check(err)
 	err = html.Execute(writer, nil)
 	check(err)
 }
 func main() {
+	// html + css
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// example from book
 	http.HandleFunc("/helloCyber", handlerCyber)
 	http.HandleFunc("/guestBook", mainPage)
+	http.HandleFunc("/guestBook/new", addSignature)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 
